4574fa02b917e9cbdfe56ee82a57bfbc16b9525d
JogfeeingVI <lifelse@outlook.com>
2025-03-27 13:57:34 +0800

    Update index.py

--- api/index.py
+++ api/index.py
@@ -2,7 +2,7 @@
 # @Author: Your name
 # @Date:   2025-03-27 12:25:13
 # @Last Modified by:   Your name
-# @Last Modified time: 2025-03-27 13:55:43
+# @Last Modified time: 2025-03-27 13:57:26
 
 from sanic import Sanic
 from sanic.response import json
@@ -13,5 +13,4 @@ app = Sanic("vercel-api-gnpng")
 async def test(request):
     return json({'info': 'hello world'})
 
-if __name__ == '__main__':
-    app.run()
+
