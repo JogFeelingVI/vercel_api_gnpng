c4f4972937002fa5f1049a1677cd6f29f3f926ac
JogfeeingVI <lifelse@outlook.com>
2025-03-27 13:47:17 +0800

    Update index.py

--- api/index.py
+++ api/index.py
@@ -2,7 +2,7 @@
 # @Author: Your name
 # @Date:   2025-03-27 12:25:13
 # @Last Modified by:   Your name
-# @Last Modified time: 2025-03-27 13:33:43
+# @Last Modified time: 2025-03-27 13:47:16
 
 from sanic import Sanic
 from sanic.response import json
@@ -10,8 +10,9 @@ from sanic.response import json
 app = Sanic("vercel-api-gnpng")
 
 @app.route('/')
-async def test(request):
-    return json({'info': 'hello world, '})
+@app.route('/<path:path>')
+async def index(request, path=""):
+    return json({'hello': path})
 
 if __name__ == '__main__':
     app.run()
