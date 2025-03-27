fabf2b4e7e9993b3b121e37258a9e4547f6508d4
JogfeeingVI <lifelse@outlook.com>
2025-03-27 14:15:53 +0800

    Update index.py

--- api/index.py
+++ api/index.py
@@ -2,16 +2,17 @@
 # @Author: Your name
 # @Date:   2025-03-27 12:25:13
 # @Last Modified by:   Your name
-# @Last Modified time: 2025-03-27 14:00:23
+# @Last Modified time: 2025-03-27 14:14:11
 
 from sanic import Sanic
-from sanic.response import json
+from sanic.response import json,text
+import os
 
 app = Sanic("vercel-api-gnpng")
 
-@app.route('/test')
-async def test(request):
-    return json({'info': 'hello world'})
+@app.route('/')
+async def index(request):
+    return text(f'info: {os.cpu_count()}')
 
 if __name__ == '__main__':
     app.run()
